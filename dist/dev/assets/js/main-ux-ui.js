1d9de1da36417c1fcbdcaeca18237ae3db97fb9b
antoineartbox <antoine.fortin@artbox.agency>
2017-10-11 13:39:44 -0400

    Adding menu correction

--- dist/dev/assets/js/main-ux-ui.js
+++ dist/dev/assets/js/main-ux-ui.js
@@ -3,12 +3,21 @@
 // Wait for the dom to be loaded
 $(document).ready(function() {
 
-    // Handle menu hover
-    // Get menu a
-    $("nav a").mouseover(function() {
+    // Handle menu display
+    $(".menu-hamburger").click(function() {
+        $("#menu").removeClass("kill-menu-display").addClass("active-menu-display");
+    });
 
-        // Find the overlay bottom item
+    $(".close-button").click(function() {
+        console.log("kill-menu");
+    });
+
+    // Handle menu hover
+    $("nav a").mouseover(function() {
+        // Find the overlay bottom item and add class
         $(this).find(".line-overlay").addClass("active");
+
+        // on current item mouse leave, remove class
         $(this).mouseleave(function() {
             $(this).find(".line-overlay").removeClass("active");
         });
--- dist/dev/site-parts/header/header_parts/menu.php
+++ dist/dev/site-parts/header/header_parts/menu.php
@@ -1,12 +1,18 @@
-<div id="menu">
-    <span class="close-button">x</span>
-    <nav>
-        <ul>
-            <li><a href="#">Services<div class="line-overlay"></div></a></li>
-            <li><a href="#">Projets<div class="line-overlay"></div></a></li>
-            <li><a href="#">À Propos<div class="line-overlay"></div></a></li>
-            <li><a href="#">Contact<div class="line-overlay"></div></a></li>
-
-        </ul>
-    </nav>
+<div id="menu-container">
+    <div class="menu-hamburger">
+        <div class="slice1"></div>
+        <div class="slice2"></div>
+        <div class="slice3"></div>
+    </div>
+    <div id="menu" class="kill-menu-display">
+        <span class="close-button">x</span>
+        <nav>
+            <ul>
+                <li><a href="">Services<div class="line-overlay"></div></a></li>
+                <li><a href="">Projets<div class="line-overlay"></div></a></li>
+                <li><a href="">À Propos<div class="line-overlay"></div></a></li>
+                <li><a href="">Contact<div class="line-overlay"></div></a></li>
+            </ul>
+        </nav>
+    </div>
 </div>
